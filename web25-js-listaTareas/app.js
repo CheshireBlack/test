7aee11d6fccd28e76dc0ba70873f0da36e097b62
Ramiro <197582@iessierradeguara.com>
2020-11-17 19:05:00 +0100

    Clase del 17 de noviembre

--- web25-js-listaTareas/app.js
+++ web25-js-listaTareas/app.js
@@ -1,3 +1,26 @@
-let listaTareas = document.getElementById("listaTareas")
-console.log(listaTareas.innerHTML)
-listaTareas.innerHTML = "<li>Hola</li>"
+let listaTareas = document.getElementById("listaTareas");
+
+function anyadirTarea() {
+    //tomo el valor de input
+    let eTarea = document.getElementById("idTarea")
+
+    //compruebo si tiene algo de texto
+    if (eTarea.value != "") {
+        //si tiene texto, añado el elemento en la lista
+        anyadirElemento(eTarea.value);
+        //despues de añadir borro el elemento input
+        eTarea.value = "";
+    }
+    //si no tiene texto, no hago nada
+}
+
+//funcion añadir elemento a la lista
+//cogera un pàrámetro y lo añadirá a la listaTareas
+function anyadirElemento(textoTarea) {
+    //cojo el elemento de la lista
+    let listaTareas = document.getElementById("listaTareas")
+    //le añado un elemento de la lista con el texto
+    //del parametro textoTarea
+    listaTareas.innerHTML += "<li>" + textoTarea + "</li>";
+}
+
