0877f6b93c1f1b49d8d9bae1677cf82f07eae034
CheshireBlack <197582@iessierradeguara.com>
2020-12-02 17:15:47 +0100

    Clase del 2 de Diciembre

---
+++ web27-js-bucles2/app.js
@@ -0,0 +1,20 @@
+let numero = 1
+
+while (numero <= 4) {
+    console.log(numero);
+    numero++
+}
+
+for (let index = 0; index < 11; index++) {
+    console.log(index);
+
+}
+
+let alumnos = ["Banu", "Miguel", "Fernando", "Luis", "Ginel", "Steeven", "Sergio","Iván"]
+
+
+for (let i = 0; i < alumnos.length; i++) {
+    console.log(alumnos[i]);
+
+}
+alert("Hemos acabado");
